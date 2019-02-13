bc96fadd25abe56cae6c2405fd31b7123c180843
cameron-yee <yee.camero23@gmail.com>
2019-02-12 17:36:55 -0700

    Added Netlify CMS NPM packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,9 +60,10 @@ module.exports = {
         siteSpeedSampleRate: 10,
         cookieDomain: "bscs.org",
       },
-    }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
+    `gatsby-plugin-netlify-cms`,
   ],
 }
