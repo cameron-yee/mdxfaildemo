aad02469344f6921ed000212bb330090bd00db10
J. S. Nickerson <jonathan.s.nickerson@gmail.com>
2019-02-12 23:28:01 -0700

    Test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,29 +3,29 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
-import React from 'react'
-import posed, { PoseGroup } from 'react-pose'
+// import React from 'react'
+// import posed, { PoseGroup } from 'react-pose'
 
-const transitionDuration = 300
-const transitionDelay = 350
+// const transitionDuration = 300
+// const transitionDelay = 350
 
-const Transition = posed.div({
-  enter: {
-    opacity: 1,
-    transition: { duration: transitionDuration },
-    delay: transitionDelay,
-    beforeChildren: true
-  },
-  exit: { opacity: 0, transition: { duration: transitionDuration } }
-})
+// const Transition = posed.div({
+//   enter: {
+//     opacity: 1,
+//     transition: { duration: transitionDuration },
+//     delay: transitionDelay,
+//     beforeChildren: true
+//   },
+//   exit: { opacity: 0, transition: { duration: transitionDuration } }
+// })
 
-export const replaceComponentRenderer = ({ props, ...other }) => {
-  const { component } = props.pageResources
-  return (
-    <PoseGroup>
-      <Transition key={props.location.key}>
-        {React.createElement(component, props)}
-      </Transition>
-    </PoseGroup>
-  )
-}
+// export const replaceComponentRenderer = ({ props, ...other }) => {
+//   const { component } = props.pageResources
+//   return (
+//     <PoseGroup>
+//       <Transition key={props.location.key}>
+//         {React.createElement(component, props)}
+//       </Transition>
+//     </PoseGroup>
+//   )
+// }
