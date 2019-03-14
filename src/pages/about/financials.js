4426ccb53e3d1c79008a5e038d2c67422c7ea4b8
cameron-yee <yee.camero23@gmail.com>
2019-03-14 16:28:50 -0600

    Added Our Story and BSCS 5Es Instructional Model pages

--- src/pages/about/financials.js
+++ src/pages/about/financials.js
@@ -57,84 +57,82 @@ const FinancialsPage = class extends Component {
 
   render() {
     return (
-      <>
-        <Layout location={this.props.location}>
-          <SEO title="Financials" />
-          <Container>
-            <PageTitle title="Financials" />
-            <Row style={{ marginBottom: '2rem' }}>
-              <Col xs={12} lg={6}>
-                <img src="/assets/ar-2017.jpg" alt="BSCS Science Learning Annual Report 2017" style={{width:"100%", marginBottom: '1rem'}} />
-              </Col>
-              <Col xs={12} lg={6}>
-                <p>
-                  In 2017, we continued advancing our research, teacher professional learning, leadership development, and instructional materials work. Included in this annual report, you will find a small sample of the 40+ projects BSCS started, furthered, or completed in 2017.
-                </p>
-                <a
-                  href="https://media.bscs.org/bscsmw/financials/annual-reports/bscs_2017_annual_report.pdf"
-                  target="_blank"
-                  rel="noopener noreferrer"
+      <Layout location={this.props.location}>
+        <SEO title="Financials" />
+        <Container>
+          <PageTitle title="Financials" />
+          <Row style={{ marginBottom: '2rem' }}>
+            <Col xs={12} lg={6}>
+              <img src="/assets/ar-2017.jpg" alt="BSCS Science Learning Annual Report 2017" style={{width:"100%", marginBottom: '1rem'}} />
+            </Col>
+            <Col xs={12} lg={6}>
+              <p>
+                In 2017, we continued advancing our research, teacher professional learning, leadership development, and instructional materials work. Included in this annual report, you will find a small sample of the 40+ projects BSCS started, furthered, or completed in 2017.
+              </p>
+              <a
+                href="https://media.bscs.org/bscsmw/financials/annual-reports/bscs_2017_annual_report.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Button variant="outline-secondary">Download 2017 Annual Report PDF</Button>
+              </a>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Table responsive striped hover>
+                <tbody>
+                  <tr>
+                    <td><a href="https://media.bscs.org/bscsmw/financials/audits/bscs_2017_133_audit_report.pdf" target="_blank" rel="noopener noreferrer">BSCS 2017 133 Audit Report</a></td>
+                  </tr>
+                  <tr>
+                    <td><a href="https://media.bscs.org/bscsmw/financials/audits/audit_report_and_financial_statements.pdf" target="_blank" rel="noopener noreferrer">BSCS 2017 Financial Statements</a></td>
+                  </tr>
+                  <tr>
+                    <td><a href="https://media.bscs.org/bscsmw/financials/audits/bscs_bod_conflict_of_interest_and_confidentiality_statement.pdf" target="_blank" rel="noopener noreferrer">BSCS BoD Conflict of Interest and Confidentiality Statement</a></td>
+                  </tr>
+                  <tr>
+                    <td><a href="https://media.bscs.org/bscsmw/financials/audits/bscs_employee_conflict_of_interest_policy.pdf" target="_blank" rel="noopener noreferrer">BSCS Employee Conflict of Interest Policy</a></td>
+                  </tr>
+                  <tr>
+                    <td><a href="https://media.bscs.org/bscsmw/financials/audits/bscs_indirect_cost_rate_agreement_2017.pdf" target="_blank" rel="noopener noreferrer">BSCS 2017 Indirect Cost Rate</a></td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Col>
+          </Row>
+          <Row style={{marginBottom: '2rem'}}>
+            <Col>
+              <hr />
+
+              <h3>Download Additional Annual Reports</h3>
+
+              <div className="d-flex">
+                <Dropdown id="dropdown" className="p-2">
+                  <Dropdown.Toggle variant="outline-primary" drop={'down'}>{this.state.year}</Dropdown.Toggle>
+                  <Dropdown.Menu flip={false}>
+                  {
+                    this.years.map(year => {
+                      return (
+                        <Dropdown.Item id={`pdf-${year}`} key={year} onClick={() => this.setUrl(year)}>{year}</Dropdown.Item>
+                      )
+                    })
+                  }
+                  </Dropdown.Menu>
+                </Dropdown>
+
+                <div
+                  id="download-pdf"
+                  className="p-2"
+                  onClick={this.openPDF}
                 >
-                  <Button variant="outline-secondary">Download 2017 Annual Report PDF</Button>
-                </a>
-              </Col>
-            </Row>
-            <Row>
-              <Col>
-                <Table responsive striped hover>
-                  <tbody>
-                    <tr>
-                      <td><a href="https://media.bscs.org/bscsmw/financials/audits/bscs_2017_133_audit_report.pdf" target="_blank" rel="noopener noreferrer">BSCS 2017 133 Audit Report</a></td>
-                    </tr>
-                    <tr>
-                      <td><a href="https://media.bscs.org/bscsmw/financials/audits/audit_report_and_financial_statements.pdf" target="_blank" rel="noopener noreferrer">BSCS 2017 Financial Statements</a></td>
-                    </tr>
-                    <tr>
-                      <td><a href="https://media.bscs.org/bscsmw/financials/audits/bscs_bod_conflict_of_interest_and_confidentiality_statement.pdf" target="_blank" rel="noopener noreferrer">BSCS BoD Conflict of Interest and Confidentiality Statement</a></td>
-                    </tr>
-                    <tr>
-                      <td><a href="https://media.bscs.org/bscsmw/financials/audits/bscs_employee_conflict_of_interest_policy.pdf" target="_blank" rel="noopener noreferrer">BSCS Employee Conflict of Interest Policy</a></td>
-                    </tr>
-                    <tr>
-                      <td><a href="https://media.bscs.org/bscsmw/financials/audits/bscs_indirect_cost_rate_agreement_2017.pdf" target="_blank" rel="noopener noreferrer">BSCS 2017 Indirect Cost Rate</a></td>
-                    </tr>
-                  </tbody>
-                </Table>
-              </Col>
-            </Row>
-            <Row style={{marginBottom: '2rem'}}>
-              <Col>
-                <hr />
-
-                <h3>Download Additional Annual Reports</h3>
-
-                <div className="d-flex">
-                  <Dropdown id="dropdown" className="p-2">
-                    <Dropdown.Toggle variant="outline-primary" drop={'down'}>{this.state.year}</Dropdown.Toggle>
-                    <Dropdown.Menu flip={false}>
-                    {
-                      this.years.map(year => {
-                        return (
-                          <Dropdown.Item id={`pdf-${year}`} key={year} onClick={() => this.setUrl(year)}>{year}</Dropdown.Item>
-                        )
-                      })
-                    }
-                    </Dropdown.Menu>
-                  </Dropdown>
-
-                  <div
-                    id="download-pdf"
-                    className="p-2"
-                    onClick={this.openPDF}
-                  >
-                    <Button variant="outline-secondary">Download PDF</Button>
-                  </div>
+                  <Button variant="outline-secondary">Download PDF</Button>
                 </div>
-              </Col>
-            </Row>
-          </Container>
-        </Layout>
-      </>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Layout>
     )
   }
 }
