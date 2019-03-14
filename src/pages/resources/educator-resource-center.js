ea2405f1d5cf38e6ab4529a173c799fe1760af48
cameron-yee <yee.camero23@gmail.com>
2019-03-14 11:13:15 -0600

    Image caching placeholder load testing

--- src/pages/resources/educator-resource-center.js
+++ src/pages/resources/educator-resource-center.js
@@ -61,7 +61,7 @@ const EducatorResourceCenter = class extends Component {
   }
 
   componentDidUpdate() {
-    const cards = document.getElementsByClassName('erc-card') 
+    const cards = document.getElementsByClassName('erc-card-image') 
     console.log(cards)
     for(let i = 0; i < cards.length; i++) {
       if(cards[i].complete) {
