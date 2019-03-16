a781a5139264e24d58b1eee9075aac66fb7e3c53
J. S. Nickerson <jonathan.s.nickerson@gmail.com>
2019-03-15 22:09:38 -0600

    Redesigning R&D page.

--- src/pages/our-work/rd-programs.js
+++ src/pages/our-work/rd-programs.js
@@ -7,7 +7,9 @@ import Layout from '../../components/layout/layout'
 
 import Button from 'react-bootstrap/Button'
 import Col from 'react-bootstrap/Col'
-import Container from 'react-bootstrap/Container';
+import Container from 'react-bootstrap/Container'
+import Card from 'react-bootstrap/Card'
+// import CardColumns from 'react-bootstrap/CardColumns'
 import Row from 'react-bootstrap/Row'
 
 import PageTitle from '../../components/layout/page-title/page-title'
@@ -15,10 +17,10 @@ import SearchBy from '../../components/atoms/search-by/search-by'
 import FilterByDropdown from '../../components/molecules/filter-by/filter-by-dropdown/filter-by-dropdown'
 import FilterByRow from '../../components/molecules/filter-by/filter-by-row/filter-by-row'
 
-import '../../global-scss/index.scss';
-import './rd-programs.scss';
+import '../../global-scss/index.scss'
+import './rd-programs.scss'
 
-import ReactPlaceholder from 'react-placeholder'
+// import ReactPlaceholder from 'react-placeholder'
 import 'react-placeholder/lib/reactPlaceholder.css'
 
 
@@ -105,92 +107,72 @@ const RDPrograms = class extends Component {
           </section>
           <section className="section" style={{ marginBottom: '4rem' }}>
             <Container>
-              {
-                this.programs.map((edge, index) => {
-                  let data_filter = JSON.parse(JSON.stringify(edge.node.frontmatter))
-                  data_filter['excerpt'] = edge.node.excerpt
-                  return(
-                    <div id="parent" key={edge.node.id}>
-                      <div id={`resource-${index}`} data-filter={JSON.stringify(data_filter)} data-type={edge.node.frontmatter.type}>
-                        <hr />
-                        <Row style={{ marginBottom: '1rem' }}>
-                          <Col sm={3} lg={2} className="d-none d-md-block">
-                            {/* {!this.state.imagesLoaded && */}
-                              <ReactPlaceholder
-                                type='rect'
-                                ready={this.state.imagesLoaded}
-                                color='rgb(41, 52, 118)'
-                                showLoadingAnimation={true}
-                                style={{width: '160px', height: '160px', borderRadius: '4px'}}
-                              >
-                                <img
-                                  className="img-fluid rd-image"
-                                  src={edge.node.frontmatter.image}
-                                  alt={edge.node.frontmatter.alt}
-                                  style={{
-                                    borderRadius: '4px'
-                                  }}
-                                />
-                              </ReactPlaceholder>
-                            {/* } */}
-                            <img
-                              className="img-fluid rd-image"
-                              src={edge.node.frontmatter.image}
-                              alt={edge.node.frontmatter.alt}
-                              onLoad={this.loaded}
+              <Row className="justify-content-center justify-content-md-start">
+              {/* <Row> */}
+                {
+                  this.programs.map((edge, index) => {
+                    let data_filter = JSON.parse(JSON.stringify(edge.node.frontmatter))
+                    data_filter['excerpt'] = edge.node.excerpt
+                    return(
+                      <Col
+                        xs={10}
+                        md={6}
+                        lg={4}
+                        className="rrc-card-col"
+                        key={edge.node.id}
+                      >
+                        <Card
+                          id={`resource-${index}`}
+                          data-filter={JSON.stringify(data_filter)} 
+                          data-type={edge.node.frontmatter.type}
+                          className="h-100"
+                          style={{
+                            borderColor: 'rgb(41, 52, 118)'
+                          }}
+                        >
+                          <Card.Img
+                            variant="top"
+                            src={edge.node.frontmatter.image}
+                            alt={edge.node.frontmatter.alt}
+                            onLoad={this.loaded}
+                            style={{ padding: '1rem 4.25rem 0' }}
+                          />
+                          <Card.Body>
+                            <Card.Title
                               style={{
-                                // marginBottom: '1rem',
-                                display: 'none',
-                                borderRadius: '4px'
+                                marginBottom: '1.5rem'
                               }}
-                            />
-                          </Col>
-                          <Col>
-                            <h3>{edge.node.frontmatter.title}</h3>
-                            <p>{edge.node.excerpt}</p>
+                            >
+                              {edge.node.frontmatter.title}
+                            </Card.Title>
+                            <Card.Text>
+                              {edge.node.excerpt}
+                            </Card.Text>
+                          </Card.Body>
+                          <Card.Footer
+                            style={{
+                              background: 'white',
+                              borderTop: 'none'
+                            }}
+                          >
                             <div className="d-flex">
-                              <div className="ml-auto button-wrapper">
+                              <div className="ml-auto align-self-end">
                                 <Link
                                   to={`/our-work/rd-programs/${edge.node.frontmatter.title.replace(/\s/g, '-').replace(/[^a-zA-Z0-9-]/g, '').toLowerCase()}`}
                                 >
-                                  <Button variant="outline-secondary" style={{ width: '124px' }}>Read More</Button>
+                                  <Button variant="outline-secondary">
+                                    Read More
+                                  </Button>
                                 </Link>
                               </div>
                             </div>
-                          </Col>
-                        </Row>
-                        <Row>
-                          <Col>
-                            <div className="d-flex">
-                              <div className="mr-auto">
-                                { 
-                                  edge.node.frontmatter.areas.map((area, index) => {
-                                    const variants = {
-                                      "Teacher Professional Learning": "primary",
-                                      "Instructional Materials Development": "secondary",
-                                      "Research": "success",
-                                      "Leadership Development": "danger"
-                                    }
-                                    return(
-                                      <span
-                                        key={index}
-                                        className={`rd-pill badge badge-pill badge-${variants[area]}`}
-                                        style={{ marginRight: '.5rem' }}
-                                      >
-                                        {area}
-                                      </span>
-                                    )
-                                  })
-                                }
-                              </div>
-                            </div>
-                          </Col>
-                        </Row>
-                      </div>
-                    </div>
-                  )
-                }) 
-              }
+                          </Card.Footer>
+                        </Card>
+                      </Col>
+                    )
+                  }) 
+                }
+              </Row>
             </Container>
           </section>
         </Layout>
