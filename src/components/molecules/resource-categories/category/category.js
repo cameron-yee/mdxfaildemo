fd5850aa6e9a917fd81f28627d63ce7989126628
J. S. Nickerson <jonathan.s.nickerson@gmail.com>
2019-03-19 23:46:48 -0600

    Modified ERC category icons and styles. Looks much nicer now.

--- src/components/molecules/resource-categories/category/category.js
+++ src/components/molecules/resource-categories/category/category.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 
 import { Link } from 'gatsby'
 
+import './category.scss'
+
 
 const Category = class extends Component {
   setCategoryFilter = (e, title) => {
@@ -11,19 +13,23 @@ const Category = class extends Component {
   render() {
     return (
       <Link
-        className="d-flex flex-column justify-content-center"
         id={this.props.category.title.toLowerCase().replace(/ /g, "-")}
         to={this.props.category.path}
         onClick={(e) => this.setCategoryFilter(e, this.props.category.title)}
       >
-        <div className="d-flex flex-column">
-          <div className="d-flex flex-row justify-content-center">
-            <img className="mb-3 mb-md-0 mb-lg-3 mt-lg-2" style={{ width: '100%' }} src={this.props.category.image} alt={this.props.category.alt} />
-          </div>
-          <div className="d-flex flex-row justify-content-center">
-            <div className="dot d-inline-flex mr-2"></div>
-            <div className="dot d-inline-flex mr-2"></div>
-            <div className="dot d-inline-flex"></div>
+        <div className="categoryImageParent">
+          <div
+            className="d-flex flex-row justify-content-center"
+          >
+              <div
+                className="rounded-circle p-3 categoryImageChild"
+              >
+                <img
+                  className="categoryImageGrandChild"
+                  src={this.props.category.image}
+                  alt={this.props.category.alt}
+                />
+              </div>
           </div>
           <div className="d-flex flex-row justify-content-center">
             <p className="mt-3 mt-md-3 mt-lg-3 mb-md-0" style={{textAlign: 'center', fontSize: '1.5rem', lineHeight: '1.1'}}>{this.props.category.title.split(" ")[0]}<br />{this.props.category.title.split(" ")[1]}</p>
