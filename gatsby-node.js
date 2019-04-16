5ebf981daf4bc95d541f8bf2186b123fa44cf89d
J. S. Nickerson <jonathan.s.nickerson@gmail.com>
2019-04-16 04:27:50 -0600

    redirects

--- gatsby-node.js
+++ gatsby-node.js
@@ -34,58 +34,50 @@ exports.createPages = ({ graphql, actions }) => {
   const { createPage, createRedirect } = actions
 
   createRedirect({
+    fromPath: 'https://bscsmw.netlify.com/',
+    toPath: 'https://bscs.org/',
+    isPermanent: true
+  })
+
+  createRedirect({
     fromPath: '/fivetools',
     toPath: '/resources/educator-resource-center/five-tools-and-processes-for-translating-the-ngss',
-    // redirectInBrowser: true,
-    // statusCode: 200
     isPermanent: true
   })
 
   createRedirect({
     fromPath: '/fivetools/',
     toPath: '/resources/educator-resource-center/five-tools-and-processes-for-translating-the-ngss',
-    // redirectInBrowser: true,
-    // statusCode: 200
     isPermanent: true
   })
 
   createRedirect({
     fromPath: '/humane-genetics',
     toPath: '/our-work/rd-programs/towards-a-more-humane-genetics-education',
-    // redirectInBrowser: true,
-    // statusCode: 200
     isPermanent: true
   })
 
   createRedirect({
     fromPath: '/humane-genetics/',
     toPath: '/our-work/rd-programs/towards-a-more-humane-genetics-education',
-    // redirectInBrowser: true,
-    // statusCode: 200
     isPermanent: true
   })
 
   createRedirect({
     fromPath: '/STeLLA',
     toPath: '/our-work/rd-programs/stella-science-teachers-learning-from-lesson-analysis',
-    // redirectInBrowser: true,
-    // statusCode: 200
     isPermanent: true
   })
 
   createRedirect({
     fromPath: '/STeLLA/',
     toPath: '/our-work/rd-programs/stella-science-teachers-learning-from-lesson-analysis',
-    // redirectInBrowser: true,
-    // statusCode: 200
     isPermanent: true
   })
 
   createRedirect({
     fromPath: '/stella',
     toPath: '/our-work/rd-programs/stella-science-teachers-learning-from-lesson-analysis',
-    // redirectInBrowser: true,
-    // statusCode: 200
     isPermanent: true
   })
 
