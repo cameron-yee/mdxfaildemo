c4588ed88518dcfc2332760c7d37cb0f8e47f03a
cameron-yee <yee.camero23@gmail.com>
2019-04-17 16:46:47 -0600

    Added credit card payment form with stripe.js and login form; Also added login and payment pages for testing

---
+++ src/components/atoms/forms/login-form/login-form.js
@@ -0,0 +1,221 @@
+import React, { Component } from 'react'
+
+import axios from 'axios'
+
+import Alert from 'react-bootstrap/Alert'
+import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Form from 'react-bootstrap/Form'
+// import Modal from 'react-bootstrap/Modal'
+import Row from 'react-bootstrap/Row'
+
+const LoginForm = class extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: undefined,
+      email_touched: false,
+      password: undefined,
+      password_touched: undefined,
+
+      loading: false,
+      notificationShow: false,
+      sent: false,
+      showErrorNotification: false,
+      errors: false
+    }
+
+    this.cancelToken = axios.CancelToken.source()
+  }
+
+  componentWillUnmount() {
+    try {
+      this.cancelToken.cancel()
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  showNotification = () => {
+    this.setState({notificationShow: true})
+  }
+
+  hideNotification = () => {
+    this.setState({notificationShow: false})
+  }
+
+  showErrorNotification = () => {
+    this.setState({showErrorNotification: true})
+  }
+
+  hideErrorNotification = () => {
+    this.setState({showErrorNotification: false})
+  }
+
+  setEmail = (e) => {
+    e.preventDefault()
+    let input_elem = document.getElementById('login-email-input');
+    /[\w]+[@][\w]+[.][\w]+/.test(input_elem.value) === false ? this.setState({email: undefined}) : this.setState({email: input_elem.value})
+  }
+
+  blurEmail = (e) => {
+    e.preventDefault()
+    this.setState({email_touched: true})
+  }
+
+  setPassword = (e) => {
+    e.preventDefault()
+    let input_elem = document.getElementById('login-password-input');
+    input_elem.value === '' ? this.setState({password: undefined}) : this.setState({password: input_elem.value})
+  }
+
+  blurFirstName = (e) => {
+    e.preventDefault()
+    this.setState({password_touched: true})
+  }
+
+
+  login = (e) => {
+    e.preventDefault()
+    this.setState({loading: true})
+
+    axios({
+      // url: 'http://127.0.0.1:8888/post-contact-form',
+      url: 'http://127.0.0.1:4000',
+      method: 'post',
+      cancelToken: this.cancelToken.token,
+      data: {
+        query: `
+          mutation {
+            login(email: "${this.state.email}", password: "${this.state.password}") {
+              user {
+                id,
+                email
+              }
+            }
+          }
+          `
+      }
+    })
+    .then(response => {
+      console.log(response);
+      if(response.status === 200) {
+          this.setState({notificationShow: true, loading: false, sent: true});
+      }
+    })
+    .catch(error => {
+      if(axios.isCancel(error)) {
+        console.log(`Request canceled: ${error}`);
+        this.setState({errors: true, showErrorNotification: true});
+      } else {
+        console.log(error);
+        this.setState({errors: true, showErrorNotification: true});
+      }
+    })
+  }
+
+  render() {
+    return (
+      // <Modal
+      //   {...this.props}
+      //   size="lg"
+      //   aria-labelledby="login-form"
+      //   centered
+      // >
+      // <Modal.Header closeButton>
+      //   <Modal.Title id="login-form">
+      //     Login Form
+      //   </Modal.Title>
+      // </Modal.Header>
+      // <Modal.Body>
+      <React.Fragment>
+        <Form>
+          <Row>
+            <Col xs={12}>
+              <Form.Group>
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  className="form-control"
+                  id="login-email-input"
+                  type="email"
+                  placeholder=""
+                  onKeyUp={this.setEmail}
+                  onBlur={this.blurEmail}
+                  isInvalid={this.state.email_touched && (!this.state.email || this.state.email === '')}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a valid email address.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Col>
+            <Col xs={12}>
+              <Form.Group>
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  className="form-control"
+                  id="login-password-input"
+                  type="password"
+                  placeholder=""
+                  maxLength="50"
+                  onKeyUp={this.setPassword}
+                  onBlur={this.blurPassword}
+                  isInvalid={this.state.password_touched && (!this.state.password || this.state.password === '')}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please provide a password.
+                </Form.Control.Feedback>
+              </Form.Group>
+            </Col>
+          </Row>
+        </Form>
+      {/* </Modal.Body>
+      <Modal.Footer> */}
+        <Container>
+          <Row>
+            <Col xs={12}>
+              <Alert show={this.state.notificationShow} onClose={this.hideNotification} dismissible variant="success">
+                Logged in!
+              </Alert>
+              <Alert show={this.state.showErrorNotification} onClose={this.hideErrorNotification} dismissible variant="danger">
+                Unable to login.
+              </Alert>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={12}>
+              <div className="d-flex justify-content-end">
+                { !this.state.errors && !this.state.loading && !this.state.sent
+                  && (!this.state.email
+                  || !this.state.password)
+                  &&
+                  <Button variant="outline-primary" disabled>Login</Button>
+                }
+                { !this.state.errors && !this.state.loading && !this.state.sent
+                  && this.state.email
+                  && this.state.password
+                  &&
+                  <Button variant="outline-primary" onClick={this.login}>Login</Button>
+                }
+                { !this.state.errors && this.state.loading &&
+                  <Button variant="outline-success" disabled>
+                    <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+                    Logging in...
+                  </Button>
+                }
+                { !this.state.errors && !this.state.loading && this.state.sent &&
+                  <Button variant="outline-success" disabled>Logged In</Button>
+                }
+                { this.state.errors &&
+                  <Button variant="outline-danger" disabled>Error</Button>
+                }
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </React.Fragment>
+    )
+  }
+}
+
+export default LoginForm
---
+++ src/pages/TESTING/payment.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react'
+import SEO from '../../components/seo'
+
+import Button from 'react-bootstrap/Button'
+// import Card from 'react-bootstrap/Card'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import Layout from '../../components/layout/layout'
+import PageTitle from '../../components/layout/page-title/page-title'
+import Row from 'react-bootstrap/Row'
+
+import '../../global-scss/index.scss'
+
+import { Elements, StripeProvider } from 'react-stripe-elements'
+import CardPaymentForm from '../../components/atoms/forms/card-payment-form/card-payment-form'
+
+const PaymentPage = class extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      stripe: null
+    }
+  }
+
+  componentDidMount() {
+    try {
+      const stripeJs = document.createElement('script');
+      stripeJs.async = true;
+      stripeJs.id = "stripe-js"
+      stripeJs.src = 'https://js.stripe.com/v3/';
+      document.getElementsByTagName('head')[0].appendChild(stripeJs)
+      console.log('hit')
+      // The setTimeout lets us pretend that Stripe.js took a long time to load
+      // Take it out of your production code!
+      setTimeout(() => {
+        this.setState({stripe: window.Stripe('pk_test_TbAwjfiPhymqoFVFe7ciXbZE')})
+      }, 500)
+    } catch(error) {
+      console.log(error)
+    }
+  }
+
+  render() {
+    if(!this.state.stripe) {
+      return (<p>Loading</p>)
+    } else {
+      return (
+        <React.Fragment>
+          <SEO
+            title="Payment"
+            description=""
+            canonical="https://bscs.org/payment"
+          />
+          <Layout location={this.props.location}>
+            <Container>
+              <PageTitle title="Payment" />
+              <Row style={{marginBottom: '1rem'}} className="d-flex flex-wrap-reverse">
+                <Col md={4} className="p-2">
+                  <StripeProvider stripe={this.state.stripe}>
+                    <Elements>
+                      <CardPaymentForm />
+                    </Elements>
+                  </StripeProvider>
+                </Col>
+              </Row>
+            </Container>
+          </Layout>
+        </React.Fragment>
+      )
+    }
+  }
+}
+
+export default PaymentPage
